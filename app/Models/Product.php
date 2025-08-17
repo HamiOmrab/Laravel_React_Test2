b2a21491de2b8fa30e1c51928669bb49b4a0660b
HamiOmrab <apprendrefran43@gmail.com>
2025-08-17 07:41:52 -0400

    Add Product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function up()
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->decimal('price', 8, 2);
+        $table->timestamps();       // created_at و updated_at
+    });
+}
+}
